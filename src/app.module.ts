a20ef35d0d287d1ad22c2ecc1c39251909f8b3d4
mrprince998 <peincerana234@gmail.com>
2026-05-16 01:12:26 +0545

    refactor(app): update app module and remove old docs
    - Update app.module.ts with new module imports
    - Remove docs/DATABASE_STRUCTURE.md (outdated)

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,16 @@ import { envSchema } from './core/config/env.schema';
 import { RateLimiterMiddleware } from './core/middleware/rate-limiter.middleware';
 import { AuthModule } from './modules/auth/auth.module';
 import { DatabaseModule } from './core/database/database.module';
+import { UsersModule } from './modules/users/users.module';
+import { RolesModule } from './modules/roles/roles.module';
+import { APP_GUARD } from '@nestjs/core';
+import {
+  AuthGuard,
+  PermissionGuard,
+  RolesGuard,
+} from './core/guards/index.guard';
+import { ProductsModule } from './modules/products/products.module';
+import { ProductCategoryModule } from './modules/product-category/product-category.module';
 
 @Module({
   imports: [
@@ -19,14 +29,20 @@ import { DatabaseModule } from './core/database/database.module';
       },
     }),
 
-    // Authentication Module
     AuthModule,
-
-    // Database Module
     DatabaseModule,
+    UsersModule,
+    RolesModule,
+    ProductsModule,
+    ProductCategoryModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_GUARD, useClass: AuthGuard },
+    { provide: APP_GUARD, useClass: RolesGuard },
+    { provide: APP_GUARD, useClass: PermissionGuard },
+  ],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
